95654a3cfd8643fa802dbc749f3544565ea8ecb3
wmilk <73866482+yoonoomilk@users.noreply.github.com>
2023-11-19 10:23:26 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// const regex = "https:\/\/www\.youtube\.com\/shorts\/*";
+// const loc = location.href;
+// if(loc.match(regex)) {
+//     location.href = loc.replace("https://www.youtube.com/shorts/", "https://www.youtube.com/watch/")
+// }
+location.href = location.href.replace("https://www.youtube.com/shorts/", "https://www.youtube.com/watch/")
